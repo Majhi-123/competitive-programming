1d3556c2ffa8348ca7fa858bb193e9e26b9617cd
Majhi-123 <95409226+Majhi-123@users.noreply.github.com>
2025-11-03 21:24:55 +0530

    Update encode_all.cpp

--- practice/encode_all.cpp
+++ practice/encode_all.cpp
@@ -1,4 +1,5 @@
-Bob wants to send an encrypted string consisting of only lowercase Latin alphabets to Alice. He has thought of using the Caesar cipher, but with a small modification. Caesar cipher is a substitution based cipher in which each letter in the plaintext is shifted a certain number of places down the alphabet. With the modification, instead of shifting all letters by the same number, Bob shifts each letter by different numbers based on a keyword K. We will understand his algorithm by an example given below.
+//razorpay
+1)Bob wants to send an encrypted string consisting of only lowercase Latin alphabets to Alice. He has thought of using the Caesar cipher, but with a small modification. Caesar cipher is a substitution based cipher in which each letter in the plaintext is shifted a certain number of places down the alphabet. With the modification, instead of shifting all letters by the same number, Bob shifts each letter by different numbers based on a keyword K. We will understand his algorithm by an example given below.
 
 Let's take the keyword K as ‘algo’ and the main word as ‘cplusplus’.
 
@@ -44,6 +45,86 @@ int main(){
         }
 
         cout<<ss<<"\n";
+    }
+    return 0;
+}
+2)You are given an array of N non-negative integers and another integer P. Write a program to find the maximum rate R, and the number of such subarrays C which has that maximum Rate.
+The rate R of a contiguous subarray is defined by the relation R = (Sum of the subarray) % P.
+
+Input Format
+The first line has two integers N and P.
+The second line has N positive integers separated by a single white space denoting array A.
+
+Output Format
+Print two integers R - maximum rate, and C - count, separated by a single white space.
+
+Constraints
+1 ≤ N, P ≤ 2 x 105
+0 ≤ A[i] ≤ 104
+
+    // Write your code here
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t=1;
+  //  cin>>t;
+    while(t--){
+      int n,p;
+      cin>>n>>p;
+      
+      vector<int> arr(n),sum(n,0);
+      for(int i=0;i<n;i++){
+        cin>>arr[i];
+        sum[i] = arr[i]%p;;
+        if(i>=1){
+            sum[i] = (sum [i] + sum[i-1])%p;
+        }
+      }
+    set<int> st;
+    int maxi =-1;
+    for(int i=0;i<n;i++){
+        if(st.empty()){
+            maxi = max(maxi,sum[i]%p);
+            st.insert(sum[i]);
+            continue;
+        }
+        else{
+           // if()
+            int x = *st.begin();
+            if(st.upper_bound(sum[i])!=st.end()){
+                 int y = *st.upper_bound(sum[i]);
+                 maxi = max(maxi, p - (y-sum[i]));
+            }
+            
+            if(x<sum[i]){
+               maxi = max(maxi, sum[i]-x);
+            }
+        }
+
+        st.insert(sum[i]);
+    }
+
+    cout<<maxi<<" ";
+    map<int,int> mp;
+    long long num = 0;
+    for(int i=0;i<n;i++){
+        if(mp[sum[i] - maxi]>0){
+            num += mp[sum[i] - maxi];
+            //length = max(length, i - mp[sum[i] - maxi]);
+        }
+        if(mp[p-(maxi - sum[i])]>0){
+            //length = max(length,i - mp[p-(maxi - sum[i])] );
+            num += mp[p-(maxi - sum[i])];
+        }
+     
+
+        mp[sum[i]]++;
+        if(sum[i]==maxi){
+            num += 1;
+        }
+    }
+    cout<<num<<"\n";
     }
     return 0;
 }
