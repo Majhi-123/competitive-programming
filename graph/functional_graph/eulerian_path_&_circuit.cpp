d21d74584ae35c8b86c3c493a9431f56a67f02e9
Majhi-123 <95409226+Majhi-123@users.noreply.github.com>
2025-12-05 00:02:36 +0530

    Update eulerian_path_&_circuit.cpp

--- graph/functional_graph/eulerian_path_&_circuit.cpp
+++ graph/functional_graph/eulerian_path_&_circuit.cpp
@@ -1,1 +1,9 @@
+//for undirecte graph--
+//Euler path- start from any node and end on any node
+//only 2 nodes have odd no. of degree and rest have even degree ------>1 for in,  1 for exit
+//Euler tour - start on one node and end on same node
+//all nodes have even no. of degrees
 
+//for directed graph--
+//Euler path- 1 node has (in_deg-out_deg) == 1 && (out_deg-in_deg)==1 , rest have in_deg == out_deg
+//Euler tour - each node has in_deg == out_deg
